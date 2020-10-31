55a237d9320ef0847332eb2e526ac1c7de88367e
JeanKnieling <jeanknieling1990@hotmail.com>
2020-10-31 02:26:44 -0300

    Avançando no curso e gostando muito de aprender JavaScript

---
+++ aula_11/ex_008.js
@@ -0,0 +1,7 @@
+var velocidade = 100
+console.log(`A velocidade do seu carro é ${velocidade}`)
+if(velocidade > 60) { // condição simples
+
+    console.log('Você ultrapassou a velocidade permitida. MULTADO POR EXCESSO DE VELOCIDADE!!!')
+}
+console.log(`Dirija sempre usando o sinto de segurança!`)
